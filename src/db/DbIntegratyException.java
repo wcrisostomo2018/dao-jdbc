25549b6dce3d18cc58ba38b0f810a3b947602aaf
Willian Bento <wbento2010@gmail.com>
2020-10-22 19:09:12 -0300

    Criando projeto e repositorio

---
+++ src/db/DbIntegratyException.java
@@ -0,0 +1,13 @@
+package db;
+
+public class DbIntegratyException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DbIntegratyException(String msg) {
+		super(msg);
+	}
+}
