57e1069b4c5c45ecc41a3ffc6cf21065bdef5dc8
Willian Bento <wbento2010@gmail.com>
2020-10-22 19:19:40 -0300

    classe departamento

---
+++ src/application/Programa.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Departamento obj = new Departamento(1,"Livros");
+		System.out.println(obj);
+
+	}
+
+}
