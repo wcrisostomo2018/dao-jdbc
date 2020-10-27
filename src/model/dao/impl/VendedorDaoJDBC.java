72fecb186ba23654230e374f0218685e8c41824b
Willian Bento <wbento2010@gmail.com>
2020-10-27 19:36:31 -0300

    VendedorDaoJDBC e daoFactory

---
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.VendedorDao;
+import model.entidades.Vendedor;
+
+public class VendedorDaoJDBC implements VendedorDao {
+
+	@Override
+	public void insert(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vendedor findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Vendedor> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
