1553e0edf7c94f63acf437d3b7e046cd345fd09a
Willian Bento <wbento2010@gmail.com>
2020-10-27 19:29:56 -0300

    Interface DepartamentoDao e VendedorDao

---
+++ src/model/dao/VendedorDao.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entidades.Vendedor;
+
+public interface VendedorDao {
+	
+	
+	void insert(Vendedor obj);
+	void update(Vendedor obj);
+	void deleteById(Integer id);
+	Vendedor findById(Integer id);
+	List<Vendedor> findAll();
+
+}
